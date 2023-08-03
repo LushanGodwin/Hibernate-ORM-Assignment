e0eb294beebb4efce22f0c27b936a388542e547b
Your Name <you@example.com>
2023-08-03 16:49:20 +0530

    add book class

---
+++ part_01/src/main/java/entity/Book.java
@@ -0,0 +1,17 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Book {
+    @Id
+    private String bookId;
+    private String name;
+}
